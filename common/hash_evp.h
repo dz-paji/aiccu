17686cb323644ece3f65517e35bc046b307b2984
dz-paji <i@paji.uk>
2024-02-22 20:16:02 +0000

    update signiture

--- common/hash_evp.h
+++ common/hash_evp.h
@@ -1,5 +1,5 @@
 #ifndef NOPROTO
-void SHA256(char message, char digest, unsigned int siglen)
+void SHA256(const char *message, char *digest, unsigned int siglen);
 #else
-void SHA256()
+void SHA256();
 #endif
